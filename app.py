3e77dd839ee579bf7c273bf0f044070c6fedc363
entbappy <entbappy73@gmail.com>
2026-09-10 22:18:21 +0600

    updated

--- app.py
+++ app.py
@@ -37,7 +37,11 @@ def startup():
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
 
 
 @app.get("/api/health")
